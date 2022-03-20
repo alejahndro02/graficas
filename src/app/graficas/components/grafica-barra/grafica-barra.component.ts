b78479005ca978fa51bfc7c601b26d0b3e158b83
alejahndro02 <alejahndro02@gmail.com>
2022-03-20 12:15:50 -0600

    se importo el compnente desde ngcharts

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, 
+         OnInit, 
+         ViewChild          } from '@angular/core';
+import { ChartConfiguration, 
+         ChartData, 
+         ChartType          } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-grafica-barra',
@@ -7,7 +13,22 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class GraficaBarraComponent implements OnInit {
+  
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+  
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+  };
+  public barChartType: ChartType = 'bar';
 
+  public barChartData: ChartData<'bar'> = {
+    labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
+    datasets: [
+      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A', backgroundColor:'#04D4AF',hoverBackgroundColor: '#67D404'},
+      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B', backgroundColor:'#3406EB',hoverBackgroundColor: '#EBE11E'}
+    ]
+  };
+  
   constructor() { }
 
   ngOnInit(): void {
