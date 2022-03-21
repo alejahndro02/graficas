05c5cfbfd30feece7c816d1772e5e26f94238fea
alejahndro02 <alejahndro02@gmail.com>
2022-03-21 09:28:35 -0600

    seagregaron los datso corspondientes

--- src/app/graficas/pages/barras-doble/barras-doble.component.ts
+++ src/app/graficas/pages/barras-doble/barras-doble.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, 
+         OnInit             } from '@angular/core';
+
+import { ChartConfiguration, 
+         ChartData,         } from 'chart.js';
 
 @Component({
   selector: 'app-barras-doble',
@@ -7,6 +11,27 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class BarrasDobleComponent implements OnInit {
+  
+  proveedoresLabels: string[] = ['2021', '2022','2023','2024','2025'];
+  
+  productosOpciones:ChartConfiguration['options']={
+    responsive:true,
+    indexAxis: 'y'
+  }
+  proveedoresData: ChartData<'bar'> = {
+    labels: this.proveedoresLabels,
+    datasets: [
+      { data: [ 100,200,300,400,500 ], label: 'Vendedor 1' },
+      { data: [ 50,250,30, 450,200 ], label: 'Vendedor 2' },
+    ]
+  }
+
+  productosData: ChartData<'bar'> = {
+    labels: this.proveedoresLabels,
+    datasets: [
+      { data: [ 200, 300,400,300, 100 ], label: 'Carros', backgroundColor: 'blue' },
+    ]
+  }
 
   constructor() { }
 
