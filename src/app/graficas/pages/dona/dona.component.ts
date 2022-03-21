48183833d959f54ede0f0550e12cbfc273b283f8
alejahndro02 <alejahndro02@gmail.com>
2022-03-21 17:28:51 -0600

    Se importo las propiedades para los valores de ngChart

--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, 
+         OnInit     } from '@angular/core';
+import { ChartData, 
+         ChartType  } from 'chart.js';
 
 @Component({
   selector: 'app-dona',
@@ -7,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class DonaComponent implements OnInit {
+  // Doughnut
+  public doughnutChartLabels: string[] = [ 'Pelotas', 'Sonajas', 'Disfraces' ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [ 350, 450, 100 ] },
 
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
   constructor() { }
 
   ngOnInit(): void {
   }
-
+    // events
+    public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+      console.log(event, active);
+    }
+  
+    public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+      console.log(event, active);
+    }
 }
