c1d9bb8d5816c3210e4a408159217941f2453431
alejahndro02 <alejahndro02@gmail.com>
2022-03-21 09:27:06 -0600

    Se agregaon los decoradores Input en los elementos deseados

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,5 @@
 import { Component, 
+         Input, 
          OnInit, 
          ViewChild          } from '@angular/core';
 import { ChartConfiguration, 
@@ -14,24 +15,29 @@ import { BaseChartDirective } from 'ng2-charts';
 })
 export class GraficaBarraComponent implements OnInit {
   
+  @Input() horizontal:boolean= false;
+  
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
   
-  public barChartOptions: ChartConfiguration['options'] = {
-    responsive: true,
+  @Input() public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true, 
+    indexAxis: 'x'
   };
   public barChartType: ChartType = 'bar';
 
-  public barChartData: ChartData<'bar'> = {
+  @Input() public barChartData: ChartData<'bar'> = {
     labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
     datasets: [
       { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A', backgroundColor:'#04D4AF',hoverBackgroundColor: '#67D404'},
       { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B', backgroundColor:'#3406EB',hoverBackgroundColor: '#EBE11E'}
     ]
   };
-  
   constructor() { }
 
   ngOnInit(): void {
+    if(this.horizontal){
+      this.barChartType = 'bar'
+    }
   }
 
 }
