1f89d111e236b2e1182d28913dec43a83b268cce
alejahndro02 <alejahndro02@gmail.com>
2022-03-22 07:36:58 -0600

    Se declara un nuevo metodo que retorna el resultado del metodo getUserSocial

--- src/app/graficas/services/graficas.service.ts
+++ src/app/graficas/services/graficas.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { delay, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,17 @@ export class GraficasService {
   }
   getDataUserSocial(){
     return this.http.get('http://localhost:3000/grafica')
+  }
+  getDataResultUserSocial(){
+    return this.getDataUserSocial()
+      .pipe(
+        delay(1500),
+        map(data => {
+          const labels= Object.keys(data)
+          const values = Object.values(data)
+          return {labels, values}
+        }
+      )
+    )
   }
 }
